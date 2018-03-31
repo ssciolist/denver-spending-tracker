b0ab6ebbe784440d9b4c6c8868b579d489fa3f46
Megan Arellano <ssciolist@gmail.com>
2018-03-31 15:25:54 -0600

    Adds a failing test for vendors

---
+++ spec/models/vendor_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe Vendor, type: :model do
+  describe 'Validations' do
+    it 'is invalid without any attributes' do
+      vendor = Vendor.new
+      expect(vendor).to be_invalid
+    end
+
+    it 'is invalid without a state' do
+      vendor = Vendor.new(name: "Benny Blancos")
+      expect(vendor).to be_invalid
+    end
+
+    it 'is invalid without a name' do
+      vendor = Vendor.new(state: "CO")
+      expect(vendor).to be_invalid
+    end
+  end
+
+  describe 'Relationships' do
+    it {should have_many(:programs)}
+    it {should have_many(:transactions)}
+  end
+end
