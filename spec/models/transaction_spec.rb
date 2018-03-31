0f38aeb11f30001bd18820438756ce42911197be
Megan Arellano <ssciolist@gmail.com>
2018-03-31 16:16:01 -0600

    failing transaction model test

---
+++ spec/models/transaction_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+describe Tranaction, type: :model do
+  describe 'Validations' do
+    it 'is invalid without a transaction date' do
+      transaction = Tranaction.new(payment_date: '12/18/2018',
+                                   description: 'zebra pens, bic pens',
+                                   amount: 50.1)
+      expect(transaction).to be_invalid
+    end
+
+    it 'is invalid without a payment date' do
+      transaction = Tranaction.new(transaction_date: '12/16/2017',
+                                   description: 'zebra pens, bic pens',
+                                   amount: 50.1)
+      expect(transaction).to be_invalid
+    end
+
+    it 'is invalid without a description' do
+      transaction = Tranaction.new(transaction_date: '12/16/2017',
+                                   payment_date: '12/18/2018',
+                                   amount: 50.1)
+      expect(transaction).to be_invalid
+    end
+
+    it 'is invalid without an amount' do
+      transaction = Tranaction.new(transaction_date: '12/16/2017',
+                                   payment_date: '12/18/2018',
+                                   description: 'zebra pens, bic pens')
+      expect(transaction).to be_invalid
+    end
+
+    it 'is valid with a transaction date, payment date, description, and amount' do
+      transaction = Tranaction.new(transaction_date: '12/16/2017',
+                                   payment_date: '12/18/2018',
+                                   description: 'zebra pens, bic pens',
+                                   amount: 50.1)
+      expect(transaction).to be_valid
+    end
+  end
+
+  describe 'Relationships' do
+    skip {should belong_to(:vendors)}
+    skip {should belong_to(:programs)}
+  end
+end
