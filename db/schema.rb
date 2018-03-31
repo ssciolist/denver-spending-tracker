539a344c641e192a44c28db86aea4e4aa4e73ac0
Megan Arellano <ssciolist@gmail.com>
2018-03-31 16:36:09 -0600

    model validations all work

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180331213620) do
+ActiveRecord::Schema.define(version: 20180331223431) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "programs", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "transactions", force: :cascade do |t|
+    t.string "transaction_date"
+    t.string "payment_date"
+    t.string "description"
+    t.float "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,37 +1,37 @@
 require 'rails_helper'
 
-describe Tranaction, type: :model do
+describe Transaction, type: :model do
   describe 'Validations' do
     it 'is invalid without a transaction date' do
-      transaction = Tranaction.new(payment_date: '12/18/2018',
+      transaction = Transaction.new(payment_date: '12/18/2018',
                                    description: 'zebra pens, bic pens',
                                    amount: 50.1)
       expect(transaction).to be_invalid
     end
 
     it 'is invalid without a payment date' do
-      transaction = Tranaction.new(transaction_date: '12/16/2017',
+      transaction = Transaction.new(transaction_date: '12/16/2017',
                                    description: 'zebra pens, bic pens',
                                    amount: 50.1)
       expect(transaction).to be_invalid
     end
 
     it 'is invalid without a description' do
-      transaction = Tranaction.new(transaction_date: '12/16/2017',
+      transaction = Transaction.new(transaction_date: '12/16/2017',
                                    payment_date: '12/18/2018',
                                    amount: 50.1)
       expect(transaction).to be_invalid
     end
 
     it 'is invalid without an amount' do
-      transaction = Tranaction.new(transaction_date: '12/16/2017',
+      transaction = Transaction.new(transaction_date: '12/16/2017',
                                    payment_date: '12/18/2018',
                                    description: 'zebra pens, bic pens')
       expect(transaction).to be_invalid
     end
 
     it 'is valid with a transaction date, payment date, description, and amount' do
-      transaction = Tranaction.new(transaction_date: '12/16/2017',
+      transaction = Transaction.new(transaction_date: '12/16/2017',
                                    payment_date: '12/18/2018',
                                    description: 'zebra pens, bic pens',
                                    amount: 50.1)
