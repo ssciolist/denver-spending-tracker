58d165d11b1d58e2155696c4e66ffc0a7a8566c5
Megan Arellano <ssciolist@gmail.com>
2018-04-02 16:49:54 -0600

    linting

--- spec/features/vendors/user_sees_all_vendors_spec.rb
+++ spec/features/vendors/user_sees_all_vendors_spec.rb
@@ -3,19 +3,19 @@ require 'rails_helper'
 describe 'User visits /vendor/:id' do
   scenario 'They see that vendor\'s information and all gov purchases' do
     vendor = Vendor.create!(name: 'Benny Blancos', state: 'CO')
-    vendor_2 = Vendor.create!(name: 'Utah Brewing Co.', state: 'UT')
-    vendor_3 = Vendor.create!(name: 'Mitt Romney Inc.', state: 'MA')
-    vendor_4 = Vendor.create!(name: 'In-and-Out', state: 'CA')
+    vendor2 = Vendor.create!(name: 'Utah Brewing Co.', state: 'UT')
+    vendor3 = Vendor.create!(name: 'Mitt Romney Inc.', state: 'MA')
+    vendor4 = Vendor.create!(name: 'In-and-Out', state: 'CA')
 
     visit vendors_path
 
     expect(page).to have_content(vendor.name)
     expect(page).to have_link(vendor.name)
-    expect(page).to have_content(vendor_2.name)
-    expect(page).to have_link(vendor_2.name)
-    expect(page).to have_content(vendor_3.name)
-    expect(page).to have_link(vendor_3.name)
-    expect(page).to have_content(vendor_4.name)
-    expect(page).to have_link(vendor_4.name)
+    expect(page).to have_content(vendor2.name)
+    expect(page).to have_link(vendor2.name)
+    expect(page).to have_content(vendor3.name)
+    expect(page).to have_link(vendor3.name)
+    expect(page).to have_content(vendor4.name)
+    expect(page).to have_link(vendor4.name)
   end
 end
--- spec/features/vendors/user_sees_one_vendor_spec.rb
+++ spec/features/vendors/user_sees_one_vendor_spec.rb
@@ -5,20 +5,20 @@ describe 'User visits /vendor/:id' do
     vendor = Vendor.new(name: 'Benny Blancos', state: 'CO')
     program = Program.new(name: 'Denver Fire Department')
     purchase = Purchase.new(transaction_date: '4/16/2017',
-                                  payment_date: '4/18/2018',
-                                  description: 'Calzones',
-                                  amount: 50.1)
-    purchase_2 = Purchase.new(transaction_date: '6/16/2017',
-                                  payment_date: '6/18/2018',
-                                  description: 'Pizza with cheese',
-                                  amount: 50.1)
-    purchase_3 = Purchase.new(transaction_date: '12/16/2017',
-                                  payment_date: '12/18/2018',
-                                  description: 'Pepperoni pizza',
-                                  amount: 50.1)
-    program_vendor_purchases = ProgramVendorPurchase.create!(program: program, vendor: vendor, purchase: purchase)
-    program_vendor_purchases = ProgramVendorPurchase.create!(program: program, vendor: vendor, purchase: purchase_2)
-    program_vendor_purchases = ProgramVendorPurchase.create!(program: program, vendor: vendor, purchase: purchase_3)
+                            payment_date: '4/18/2018',
+                            description: 'Calzones',
+                            amount: 50.1)
+    purchase2 = Purchase.new(transaction_date: '6/16/2017',
+                             payment_date: '6/18/2018',
+                             description: 'Pizza with cheese',
+                             amount: 50.1)
+    purchase3 = Purchase.new(transaction_date: '12/16/2017',
+                             payment_date: '12/18/2018',
+                             description: 'Pepperoni pizza',
+                             amount: 50.1)
+    ProgramVendorPurchase.create!(program: program, vendor: vendor, purchase: purchase)
+    ProgramVendorPurchase.create!(program: program, vendor: vendor, purchase: purchase_2)
+    ProgramVendorPurchase.create!(program: program, vendor: vendor, purchase: purchase_3)
 
     visit vendor_path(vendor)
 
@@ -29,15 +29,15 @@ describe 'User visits /vendor/:id' do
     expect(page).to have_content(purchase.transaction_date)
     expect(page).to have_content(purchase.payment_date)
     expect(page).to have_content(purchase.programs.first.name)
-    expect(page).to have_content(purchase_2.description)
-    expect(page).to have_content(purchase_2.amount)
-    expect(page).to have_content(purchase_2.transaction_date)
-    expect(page).to have_content(purchase_2.payment_date)
-    expect(page).to have_content(purchase_2.programs.first.name)
-    expect(page).to have_content(purchase_3.description)
-    expect(page).to have_content(purchase_3.amount)
-    expect(page).to have_content(purchase_3.transaction_date)
-    expect(page).to have_content(purchase_3.payment_date)
-    expect(page).to have_content(purchase_3.programs.first.name)
+    expect(page).to have_content(purchase2.description)
+    expect(page).to have_content(purchase2.amount)
+    expect(page).to have_content(purchase2.transaction_date)
+    expect(page).to have_content(purchase2.payment_date)
+    expect(page).to have_content(purchase2.programs.first.name)
+    expect(page).to have_content(purchase3.description)
+    expect(page).to have_content(purchase3.amount)
+    expect(page).to have_content(purchase3.transaction_date)
+    expect(page).to have_content(purchase3.payment_date)
+    expect(page).to have_content(purchase3.programs.first.name)
   end
 end
