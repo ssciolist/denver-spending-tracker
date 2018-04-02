8c893153ee0d30cb045f3552c1f8e4ee8991c92a
Megan Arellano <ssciolist@gmail.com>
2018-04-02 17:21:21 -0600

    passing program index and show tests

--- spec/features/programs/user_sees_all_programs_spec.rb
+++ spec/features/programs/user_sees_all_programs_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
-describe 'User visits /program/:id' do
+describe 'User visits /programs' do
   scenario 'They see that program\'s information and all gov purchases' do
     program = Program.create!(name: 'Denver Police Department')
     program2 = Program.create!(name: 'City Treasury')
     program3 = Program.create!(name: 'Parks and Rec')
     program4 = Program.create!(name: 'Public Works')
 
-    visit program_path(program)
+    visit programs_path
 
     expect(page).to have_content(program.name)
     expect(page).to have_link(program.name)
