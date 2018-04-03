1938c6fe2ccae9d4d9d255c6f605e25473487732
Megan Arellano <ssciolist@gmail.com>
2018-04-03 11:08:15 -0600

    adds user model test

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe User, type: :model do
+  describe 'Validations' do
+    it { should validate_presence_of(:username) }
+    it { should validate_uniqueness_of(:username) }
+  end
+
+  describe 'Roles' do
+    skip 'can have an admin role' do
+      admin = User.create!(username: 'Admin', password: 'pass', role: 1)
+
+      expect(admin.role).to eq('admin')
+      expect(admin.admin?).to be_truthy
+    end
+
+    skip 'can be a regular user' do
+      user = User.create!(username: 'User', password: 'word', role: 0)
+
+      expect(user.role).to eq('user')
+      expect(user.user?).to be_truthy
+    end
+
+    skip 'will default to regular user' do
+      user = User.create!(username: 'Megan', password: 'mypass')
+
+      expect(user.role).to eq('user')
+      expect(user.user?).to be_truthy
+    end
+  end
+end
