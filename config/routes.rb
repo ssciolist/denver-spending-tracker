a77113496aa08764e9a45f5d1b7a3f2025535c56
Megan Arellano <ssciolist@gmail.com>
2018-04-02 20:40:03 -0600

    adds a bunch of user session stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  root "dashboard#index"
+
+  resources :users, only: [:new, :index, :create, :show]
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  
   resources :vendors, only: [:show, :index]
   resources :programs, only: [:show, :index]
 
-  root "dashboard#index"
-  resources :users, only: [:new, :index, :create, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
