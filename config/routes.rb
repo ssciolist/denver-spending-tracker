eabdcaf67a3aca3a38e161dfc472c28a68a10a33
Megan Arellano <ssciolist@gmail.com>
2018-04-02 21:09:21 -0600

    puts in user log out behavior

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :index, :create, :show]
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  post '/logout', to: 'sessions#destroy'
+  get '/logout', to: 'sessions#destroy'
 
   resources :vendors, only: [:show, :index]
   resources :programs, only: [:show, :index]
--- spec/features/users/user_can_log_out_spec.rb
+++ spec/features/users/user_can_log_out_spec.rb
@@ -1,22 +1,20 @@
 require 'rails_helper'
 
-describe 'User visits the homepage' do
-  scenario 'they can log into their account' do
+describe 'User visits their account' do
+  scenario 'they can log out of their account' do
     user = User.create(username: 'Marglargbot', password: 'test')
 
-    visit '/'
+    visit login_path
 
-    click_on 'Log in'
-
-    expect(current_path).to eq(login_path)
     fill_in 'username', with: user.username
     fill_in 'password', with: user.password
-
     click_on 'Log me in'
 
     expect(current_path).to eq(user_path(user))
 
-    expect(page).to have_content("Welcome, #{user.username}")
-    expect(page).to have_content('Log out')
+    click_on 'Log out'
+
+    expect(current_path).to eq(root_path)
+    expect(page).to_not have_content('Marglargbot')
   end
 end
