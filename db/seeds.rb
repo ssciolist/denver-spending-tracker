b9bb42f52c5ffd425ffd2d171cc7a53dabec461c
Megan Arellano <ssciolist@gmail.com>
2018-04-04 08:08:19 -0600

    Revert "Heroku -- add rakefile and put in dupe data"

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,3 @@
-require 'CSV'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,9 +5,6 @@ require 'CSV'
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-CSV.foreach('./data/ProcurementCard_2017_dupe.csv', headers: true, header_converters: :symbol) do |row|
-  vendor = Vendor.create(name: row[:merchant_name], state: row[:merchant_state]) unless Vendor.find_by_name(row[:merchant_name])
-  program = Program.create(name: row[:program_area]) unless Program.find_by_name(row[:program_area])
-  purchase = Purchase.create!(transaction_date: row[:transaction_date], payment_date: row[:payment_date], description: row[:item_description], amount: row[:transaction_amount])
-  ProgramVendorPurchase.create(purchase: purchase, program: program, vendor: vendor)
+CSV.foreach('./data/ProcurementCard_2017.csv', headers: true, header_converters: :symbol) do |row|
+
 end
